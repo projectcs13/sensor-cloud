8a4a912569fdd9a6a2eca40d617421770456015e
gkoutsou <gkoutsou90@gmail.com>
2013-09-26 14:05:29 +0200

    split database.hrl

--- engine/src/database.erl
+++ engine/src/database.erl
@@ -11,7 +11,10 @@
 -module(database).
 -compile(export_all).
 -include_lib("stdlib/include/qlc.hrl").
--include("include/database.hrl").
+-include("include/user.hrl").
+-include("include/unique_id.hrl").
+-include("include/record.hrl").
+-include("include/stream.hrl").
 
 %% @doc
 %% Function: init/0 
