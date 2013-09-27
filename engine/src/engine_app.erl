149c2856e23ef00ec98c639ac290b92cf3d63cca
gkoutsou <gkoutsou90@gmail.com>
2013-09-27 10:25:38 +0200

    merged webmachine with our engine

---
+++ engine/src/engine_app.erl
@@ -0,0 +1,21 @@
+%% @author author <author@example.com>
+%% @copyright YYYY author.
+
+%% @doc Callbacks for the engine application.
+
+-module(engine_app).
+-author('author <author@example.com>').
+
+-behaviour(application).
+-export([start/2,stop/1]).
+
+
+%% @spec start(_Type, _StartArgs) -> ServerRet
+%% @doc application start callback for engine.
+start(_Type, _StartArgs) ->
+    engine_sup:start_link().
+
+%% @spec stop(_State) -> ServerRet
+%% @doc application stop callback for engine.
+stop(_State) ->
+    ok.
