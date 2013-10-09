4fbb8a7b2b95e99253b3a622e3da48242dcfc62e
gkoutsou <gkoutsou90@gmail.com>
2013-10-09 16:20:20 +0200

    does it work now? v2

--- Makefile
+++ Makefile
@@ -24,7 +24,7 @@ get_libs:
 test_travis: 
 	-@mkdir test-results
 	lib/elastic_search/bin/elasticsearch&
-	erl -pa ebin/ lib/*/ebin/ -boot start_sasl -s reloader -s engine -sname database -setcookie database -mnesia dir '"/home/database/Mnesia.Database"' -s database init -s test run
+	erl -pa ebin/ lib/*/ebin/ -boot start_sasl -s reloader -s engine -sname database -setcookie database -mnesia dir '"/home/database/Mnesia.Database"' -s database init -s test run_travis
 
 clean_emacs_vsn_files:
 	rm -rf *~
--- test/test.erl
+++ test/test.erl
@@ -6,7 +6,7 @@
 %% @doc Test wrapper module
 -module(test).
 -author('Tommy Mattsson').
--export([run/0]).
+-export([run/0,run_travis/0]).
 
 %% @doc
 %% Function: run/0
@@ -16,7 +16,28 @@
 %% Returns: ok | no_return()
 %% @end
 run() ->
-	timer:sleep(10000),
+    Result = eunit:test("ebin",
+			[verbose, 
+			 {cover_enabled, true},
+			 {report, {eunit_surefire, [{dir, "test-results"}]}}
+			]),
+    case Result of
+	ok ->
+	    init:stop();
+	error ->
+	    halt(1)
+    end.
+
+
+%% @doc
+%% Function: run_travis/0
+%% Purpose: Wrapper function for testing in order to be able to return a 
+%%          non-zero exit code on failure of one or more test cases fails.
+%%          This is for getting tests to work with Travis CI.
+%% Returns: ok | no_return()
+%% @end
+run_travis() ->
+	timer:sleep(15000),
     Result = eunit:test("ebin",
 			[verbose, 
 			 {cover_enabled, true},
