07ced579e6f3d58c649082fa5cdda45a2e2f365c
Tommy <tommy.mattsson@gmail.com>
2013-10-20 01:27:33 +0200

    Started on an include file for making it easier to create json string objects. It is empty right now however

---
+++ include/json.hrl
@@ -0,0 +1,17 @@
+%% @author Tommy Mattsson <toma10293@student.uu.se>
+%% [www.csproj13.student.it.uu.se]
+%% @version 1.0
+%% @copyright [Copyright information]
+%%
+%% @doc == json include file ==
+%% Provides definitions for creating JSON string objects
+%%
+%% @end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-define(SUGGESTION, "suggestion").
+
+
