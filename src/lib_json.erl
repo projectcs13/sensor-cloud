967dc1b30c22f3eb6897d94dca9b3c118e9ea8b1
Tommy <tommy.mattsson@gmail.com>
2013-10-20 23:35:38 +0200

    Added basic documentation for the functions in the module. Will add detailed usage documentation in the module later.

--- src/lib_json.erl
+++ src/lib_json.erl
@@ -33,7 +33,7 @@ encode(Json) when is_tuple(Json)->
 
 %% @doc
 %% Function: decode/1
-%% Purpose : decodes a json object. 
+%% Purpose : Decodes a json object. 
 %% Returns : Either a mochiweb, json_term(), representation of the json object
 %%           or a more readable version which only contains lists, tuples and string
 %% @end
@@ -49,10 +49,9 @@ decode({pretty, Json}) when is_list(Json)->
     
 %% @doc
 %% Function: get_field/2
-%% Purpose: Return the value of a certain field in the given JSON string.
-%% Returns: Return the value of the specified field, if it exists,                                  
-%%          otherwise returns the empty string.
-%% To-Check:  what if there is a ',' inside a Value??? It will fail?
+%% Purpose: Get the value at a certain field
+%% Returns: Return the string representation of the value of the specified 
+%%          field, if it exists, otherwise returns 'false' value.
 %% @end
 -spec get_field(String::string(), Field::string()) -> string() | atom().
 get_field(Json, Query) ->
@@ -60,19 +59,23 @@ get_field(Json, Query) ->
     mk_pretty(Result).
 
 %% @doc
-%% Function: get_field_/2
-%% Purpose: Return the value of a certain field in the given JSON string.
-%% Returns: Return the value of the specified field, if it exists,                                  
-%%          otherwise returns the empty string.
-%% To-Check:  what if there is a ',' inside a Value??? It will fail?
+%% Function: get_field_help/2
+%% Purpose: Get a value at a certain field.
+%% Returns: Return the value of the specified field, if it exists, otherwise
+%%          returns 'false' value.
 %% @end
 get_field_help(Json, Query) ->
     JsonObj = mochijson2:decode(Json),
     JsonParser = destructure_json:parse("Obj."++Query),
     JsonParser(JsonObj).
 
-
-
+%% @doc
+%% Function: get_field_value/3
+%% Purpose: Get a certain value of a certain field
+%% Returns: Returns the value of the specified field, if it exists, otherwise
+%%          returns 'false' value. Handles  wildcard searches for fields (not 
+%%          wildcard for values)
+%% @end
 get_field_value(Json, Query, Value) ->
     QueryParts = find_wildcard_fields(Query),
     case field_recursion(Json, QueryParts, Value) of
@@ -82,6 +85,12 @@ get_field_value(Json, Query, Value) ->
 	    false
     end.
 
+%% @doc
+%% Function: field_value_exists/3
+%% Purpose: Check if a specific field with a specific value exists
+%%          Handles wildcard searches for fields (not wildcard for values)
+%% Returns: Return true if the value exists, otherwise false
+%% @end
 field_value_exist(Json, Query, Value) ->
     case get_field_value(Json, Query, Value) of
 	Value ->
@@ -91,26 +100,39 @@ field_value_exist(Json, Query, Value) ->
     end.
 
 
-mk_pretty(R) when is_binary(R) ->
-    binary_to_list(R);
+%% @doc
+%% Function: mk_pretty/1
+%% Purpose:  Make a json representation more readable. The functions in the 
+%%           mochijson2 module is hard to read because it is often binaries
+%% Returns:  list, tuple and string representation of the json object
+%% @end
+mk_pretty(Json) when is_binary(Json) ->
+    binary_to_list(Json);
 
-mk_pretty(R) when is_integer(R) ->
-    integer_to_list(R);
+mk_pretty(Json) when is_integer(Json) ->
+    integer_to_list(Json);
 
-mk_pretty(R) when is_list(R) ->
-    lists:map(fun mk_pretty/1, R);
+mk_pretty(Json) when is_list(Json) ->
+    lists:map(fun mk_pretty/1, Json);
 
 mk_pretty({struct, Values})  ->
     ValuesList = lists:map(fun tuple_to_list/1, Values),
     StringValues = lists:map(fun mk_pretty/1, ValuesList),
     lists:map(fun list_to_tuple/1, StringValues);
 
-mk_pretty(R) ->
-    R.
+mk_pretty(Json) ->
+    Json.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+%% @doc
+%% Function: encode_help/1
+%% Purpose: Helper function for making a string of a tuple and list 
+%%          representation of a json object
+%% Returns: string()
+%% TODO: NOT COMPLETED YET
+%% @end
 encode_help(Json) when is_tuple(Json)->
     JsonList = tuple_to_list(Json),
     encode_help(JsonList, "").
@@ -125,7 +147,11 @@ encode_help([{Attr, Value} | Tl], Acc) when is_list(Value) ->
     
     
 
-
+%% @doc
+%% Function: field_recursion/3
+%% Purpose: Handles recursion over the specific fields in a json object 
+%% Returns: Either the found value or 'false'
+%% @end
 field_recursion(Json, QueryParts, Value) ->
     field_recursion(Json, QueryParts, Value, "").
 
@@ -165,6 +191,11 @@ field_recursion(Json, [{no_wildcard, Field}], Value, Query) ->
 	    R
     end.
 
+%% @doc
+%% Function: index_recursion/4
+%% Purpose: Handles recursion over indexes for one specific field
+%% Returns: Either the found value or 'false'
+%% @end
 index_recursion(Json, [{wildcard, Field, 0 = N} | Rest], Value, Query) ->
     NewQuery = lists:concat([Query, Field]),
     NewIndexQuery = query_index_prep(NewQuery, N),
@@ -183,7 +214,12 @@ index_recursion(Json, [{wildcard, Field, N} | Rest], Value, Query) ->
 	    index_recursion(Json, [{wildcard, Field, N-1} | Rest], Value, Query)
     end.
     
-    
+%% @doc
+%% Function: get_field_max_index/2
+%% Purpose: Makes a query for a field in order to find out the amount of items for that field.
+%%          Does NOT handle wildcard queries
+%% Returns: The length of the item list or 'false' if the field was not found
+%% @end    
 get_field_max_index(Json, Query) ->
     case get_field_help(Json, Query) of
 	R when is_list(R) ->
@@ -192,10 +228,21 @@ get_field_max_index(Json, Query) ->
 	    R
     end.
 
-    
+%% @doc
+%% Function: query_index_prep/2
+%% Purpose: Encodes the index of a field for a json query
+%% Returns: json query encoded as string()
+%% @end
 query_index_prep(Query, N) ->
     lists:concat([Query, "[", integer_to_list(N), "]"]).
 
+
+%% @doc
+%% Function: find_wildcard_fields/1
+%% Purpose:  Search the query for fields ending with [*]
+%% Returns:  A list containing the different parts of the query. Each element is
+%%           tagged as {wildcard, X} or {no_wildcard, X}
+%% @end
 find_wildcard_fields(Query) ->
     WildCards = re:split(Query, "\\[\\*\\]", [{return, list}]),
     case lists:last(WildCards) of
