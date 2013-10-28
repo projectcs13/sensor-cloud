05d8ee04af24878f59cb852fdd53d254cff53b84
Volt42 <caca7037@student.it.uu.se>
2013-10-28 10:00:29 +0100

    Added rErland dependency and changed name from prediction to analyse

--- src/analyse.erl
+++ src/analyse.erl
@@ -1,4 +1,4 @@
--module(prediction).
+-module(analyse).
 -export([get_time_series/1, get_arima_string/1]).
 
 -include("webmachine.hrl").
@@ -7,12 +7,24 @@
 
 %<<"[ { \"value\": 3347, \"date\": \"1995-06-09\" }, { \"value\": 1833, \"date\": \"1995-07-26\" }, { \"value\": 2470, \"date\": \"1996-11-19\" }, { \"value\": 2849, \"date\": \"1997-11-15\" }, { \"value\": 3295, \"date\": \"1998-10-01\" }, { \"value\": 2853, \"date\": \"1998-12-26\" }, { \"value\": 3924, \"date\": \"1999-11-23\" }, { \"value\": 1392, \"date\": \"2000-10-19\" }, { \"value\": 2127, \"date\": \"2001-03-09\" }, { \"value\": 2121, \"date\": \"2001-05-27\" }, { \"value\": 2817, \"date\": \"2002-05-03\" }, { \"value\": 1713, \"date\": \"2003-02-13\" }, { \"value\": 3699, \"date\": \"2003-05-25\" }, { \"value\": 2387, \"date\": \"2003-07-13\" }, { \"value\": 2409, \"date\": \"2004-01-11\" }, { \"value\": 3163, \"date\": \"2004-12-06\" }, { \"value\": 2168, \"date\": \"2005-10-05\" }, { \"value\": 1276, \"date\": \"2008-02-12\" }, { \"value\": 2597, \"date\": \"2009-12-29\" }, { \"value\": 2851, \"date\": \"2010-10-23\"}]">>
 
-arima_test(Json) -> 
+
+%% @doc
+%% Function: predict/1
+%% Purpose: Used to do a prediction with R given a json object
+%% Returns: List
+%% @end
+
+predict(Json) -> 
 	eri:start(),
 	eri:connect(),
 	{_Start, _End, Values} = get_time_series(Json),
-	Res = eri:eval(get_arima_string(Values)),
-	Res.
+	eri:eval("pred <-" ++ get_arima_string(Values)),
+	Mean = eri:eval("data.frame(c(pred$mean))[[1]]"),
+	Lo80 = eri:eval("data.frame(c(pred[5]))[1]"),
+	Hi80 = eri:eval("data.frame(c(pred[6]))[1]"),
+	Lo95 = eri:eval("data.frame(c(pred[5]))[2]"),
+	Hi95 = eri:eval("data.frame(c(pred[6]))[2]"),
+	{Mean, Lo80, Hi80, Lo95, Hi95}.
 	
 
 get_arima_string(Values) -> 
