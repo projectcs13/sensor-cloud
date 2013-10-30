fa6582600112b244bfe739c463a871a805067d05
Kristian Ionescu <ionescu.kristian@gmail.com>
2013-10-30 14:24:30 +0100

    add receive.js

--- frontend_scripts/receive.js
+++ frontend_scripts/receive.js
@@ -2,33 +2,49 @@
 // Create context using rabbit.js (cfr ZMQ),
 // io and the subscriber socket.
 
-var exchange = process.argv[2],
-    port = 8080,
+var port = 8080,
     context = require('rabbit.js').createContext(),
     io = require('socket.io').listen(port),
-    sub = context.socket('SUB');
-
-sub.setEncoding('utf8');
-
+    url = require('url');
+    var streams = new Object();
+    // Limits debug messages that are printed by socket.io
+    io.set('log level', 1); 
 
 // A websocket is connected (eg: browser).
 io.sockets.on('connection', function(socket) {
-    console.log("Websocket is connected.");
-    // Connect socket to updates exchange.
-    sub.connect(exchange);
-    console.log("EXCHANGE : "+exchange);   
-    // Register handler that hanles incoming data when the socket
-    // detects new data on our queues.
-    // When receiving data, it gets pushed to the connectec websocket.
-    sub.on('data', function(data) {
-        var type = data.substring(6, 15),
+    var sub = context.socket('SUB');
+    sub.setEncoding('utf8');
+    var con_url = (url.parse(socket.handshake.url, true).query.ns).split("/");
+    var namespace = con_url[con_url.length - 1];
+    if(streams[namespace] == undefined) {
+        streams[namespace] = 1;
+        sub.connect(namespace);
+        console.log("Created "+namespace+" with "+streams[namespace]+" users connected.");
+    }else {
+        streams[namespace]++;
+        console.log("Connected to "+namespace+" with "+streams[namespace]+" users connected.");
+    }
+
+    io.of('/'+namespace).on('connection', function(sock) {
+        sub.on('data', function(data) {
+            var type = data.substring(6, 15),
             time = data.substring(18, 37),
             rest = data.substring(40).split("k\0"),
-	    id = rest[0],
-	    value = rest[1].substring(1);
-            //Add support for stream_id
-        console.log("TYPE : "+type+" TIME : "+time+" VALUE : "+ value + " ID : " + id);
-
-        socket.emit(type, time + " :: " + rest);
+    	    id = rest[0],
+    	    value = rest[1].substring(1);
+            sock.send(id+" :: "+time + " :: " + value);
+            //console.log(io.sockets.clients().length);
+        });
+    });
+    socket.on('disconnect', function() {
+    streams[namespace]--;
+    
+    console.log("Disconnected from "+namespace+" with "+streams[namespace]+" users connected.");
+    if(streams[namespace] == 0) {
+        console.log("No user connected to "+namespace);
+        //streams[namespace] = undefined;
+        //sub.destroy();
+        console.log("NameSpace : "+namespace+" is "+streams[namespace]);
+    }
     });
 });
