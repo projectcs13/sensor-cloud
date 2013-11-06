ea0371910f91c518119b27bcda553a01e6d663ac
Andreas Moregard Haubenwaller <andreas.moregard@gmail.com>
2013-11-06 13:59:06 +0100

    Forgot to bind Body5

--- test/resource_tests.erl
+++ test/resource_tests.erl
@@ -115,7 +115,7 @@ get_resource_test() ->
 	{ok, {{_Version3, 200, _ReasonPhrase3}, _Headers3, Body3}} = httpc:request(get, {"http://localhost:8000/users/0/resources/" ++ lib_json:to_string(DocId), []}, [], []),
 	{ok, {{_Version4, 200, _ReasonPhrase4}, _Headers4, Body4}} = httpc:request(get, {"http://localhost:8000/users/0/resources/_search?test=get", []}, [], []),
 	%Get resource that doesn't exist
-	{ok, {{_Version5, 500, _ReasonPhrase5}, _Headers5, _Body5}} = httpc:request(get, {"http://localhost:8000/resources/1" ++ lib_json:to_string(DocId), []}, [], []),
+	{ok, {{_Version5, 500, _ReasonPhrase5}, _Headers5, Body5}} = httpc:request(get, {"http://localhost:8000/resources/1" ++ lib_json:to_string(DocId), []}, [], []),
 	{{Year,Month,Day},_} = calendar:local_time(),
     Date = generate_date([Year,Month,Day]),
     % Tests to make sure the correct creation date is added
