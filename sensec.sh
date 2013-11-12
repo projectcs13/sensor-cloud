a9adc105b667c0272dcd15c2b82523c9d5f3a645
Kristian Ionescu <ionescu.kristian@gmail.com>
2013-11-12 14:52:02 +0100

    added node.js in sensec.sh

--- sensec.sh
+++ sensec.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#Check if user has root access
 if [ "$USER" = "root" ]; then
 	if [ "$1" = "start" ]; then
 		#Create log directory if it does not already exist
@@ -6,25 +8,33 @@ if [ "$USER" = "root" ]; then
 		mkdir -p logs/"$now"
 		chmod 777 logs
 		echo "Starting up RabbitMQ-Server"
-		make run_rabbit > logs/"$now"/rabbit_log.log &
+		./lib/rabbitmq-server/scripts/rabbitmq-server > logs/"$now"/rabbit_log.log &
 		echo "Starting up ElasticSearch"
 		./lib/elasticsearch/bin/service/elasticsearch console > logs/"$now"/es_log.log &
-		echo "Starting Sensor-Cloud"
 		if [ -s .temp.log ]; then
    		   echo "Sensor cloud already running, to close run ./sensec.sh stop"
 		else
+		   sleep 2
+		   echo "Starting node.js with receive.js"
+		   nodejs javascripts/receive.js >logs/"$now"/nodejs_log.log &
+		   echo $! > .temp.log
+		   echo "Starting Sensor-Cloud"
       		   export R_HOME="/usr/lib/R"
 		   erl -noshell -pa ebin/ lib/*/ebin/ lib/*/bin/ -boot start_sasl -s reloader -s engine -sname engine > logs/"$now"/sensor-cloud_log.log &
-		   echo $! > .temp.log
+		   echo $! >> .temp.log
 		fi
 	 elif [ "$1" = "stop" ]; then
+		echo "Closing nodejs and Sensor-Cloud"
+		while read line
+		do
+			kill -15 "$line"
+		done < .temp.log
+		rm .temp.log
+
 		echo "Closing down RabbitMQ-Server"
 		./lib/rabbitmq-server/scripts/rabbitmqctl stop
 		echo "Closing down ElasticSearch"
 		./lib/elasticsearch/bin/service/elasticsearch stop
-		echo "Closing Sensor-Cloud"
-		kill -15 $(cat .temp.log)
-		rm .temp.log
 	fi
 else 
 	echo "$USER has no root access, use sudo"
