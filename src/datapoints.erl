3237efd2c425b755a9ab5a45fffc8a4640115b0b
Tomas Sävström <tosa7943@student.uu.se>
2013-11-18 12:38:27 +0100

    Update datapoints.erl

--- src/datapoints.erl
+++ src/datapoints.erl
@@ -275,7 +275,7 @@ update_fields_in_stream(StreamId,TimeStamp,ReqData,State) ->
             {{halt, Code}, wrq:set_resp_body(ErrorString, ReqData), State};
 		{ok,StreamJson} ->
 			OldHistorySize = lib_json:get_field(StreamJson, "_source.history_size"),
-			Json = set_attrs([{"last_update", TimeStamp} , {"history_size", OldHistorySize+1}]),
+			Json = lib_json:set_attrs([{"last_update", TimeStamp} , {"history_size", OldHistorySize+1}]),
 			Update = api_help:create_update(Json),
 			case api_help:update_doc(?INDEX, "stream", StreamId, Update) of
 				{error, {Code, Body}} -> 
