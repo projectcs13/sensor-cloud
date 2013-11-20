bd773ea54ab940da16c8c343576c7fbc45796773
Kristian Ionescu <ionescu.kristian@gmail.com>
2013-11-20 14:08:48 +0100

    updated the startup script

--- scripts/sensec.sh
+++ scripts/sensec.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 dir=`dirname $0`
 #Specify project home directory.
-HOME_PATH=`cd $dir;cd ..;pwd`
+HOME_PATH=`cd  $dir;cd ..;pwd`
 LOG_DIR=$HOME_PATH/priv/logs
 LOG_JS_DIR=$LOG_DIR
 
+echo "$HOME_PATH"
 #Check if user has root access
 if [ "$USER" = "root" ]; then
 	if [ "$1" = "start" ]; then		
@@ -15,13 +16,21 @@ if [ "$USER" = "root" ]; then
 		if [ -s $HOME_PATH/.temp.log ]; then
    		   echo "Sensor cloud already running, to close run ./sensec.sh stop"
 		else
-		   sleep 2
+		   sleep 5
 		   echo "Starting node.js with receive.js"
-		   nodejs $HOME_PATH/javascripts/receive.js > $LOG_JS_DIR/nodejs_log.log &
+		   if [ -d "$LOG_JS_DIR" ]; then
+				nodejs $HOME_PATH/javascripts/receive.js > $LOG_JS_DIR/nodejs_log.log &
+		   else
+		    	nodejs $HOME_PATH/javascripts/receive.js &
+		   fi
 		   echo $! > $HOME_PATH/.temp.log
 		   echo "Starting Sensor-Cloud"
       	   export R_HOME="/usr/lib/R"
-		   erl -noshell -env SENSOR_PATH $HOME_PATH -pa $HOME_PATH/ebin/ $HOME_PATH/lib/*/ebin/ $HOME_PATH/lib/*/bin/ -boot start_sasl -s reloader -s engine -sname engine -config $HOME_PATH/config/engine.config > $LOG_DIR/sensor-cloud_log.log &
+      	   if [ -d "$LOG_DIR" ]; then
+		   		erl -noshell -env SENSOR_PATH $HOME_PATH -pa $HOME_PATH/ebin/ $HOME_PATH/lib/*/ebin/ $HOME_PATH/lib/*/bin/ -boot start_sasl -s reloader -s engine -sname engine -config $HOME_PATH/config/engine.config > $LOG_DIR/sensor-cloud_log.log &
+		   else
+		   		erl -noshell -env SENSOR_PATH $HOME_PATH -pa $HOME_PATH/ebin/ $HOME_PATH/lib/*/ebin/ $HOME_PATH/lib/*/bin/ -boot start_sasl -s reloader -s engine -sname engine -config $HOME_PATH/config/engine.config &
+		   fi
 		   echo $! >> $HOME_PATH/.temp.log
 		fi
 	 elif [ "$1" = "stop" ]; then
