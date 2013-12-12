75fca8e31b263395182216866371314c5904c5f4
tomas <tomas@tomas>
2013-12-12 09:13:43 +0100

    Added code to give back all documents when doing a get and not giving a size

--- src/users.erl
+++ src/users.erl
@@ -413,7 +413,12 @@ get_user(ReqData, State) ->
                     % Get all users
                     case wrq:get_qs_value("size",ReqData) of 
                         undefined ->
-                            Size = 100;
+							case erlastic_search:count_type(?INDEX, "user") of
+								{error, {_CountCode, _CountBody}} -> 
+									Size = 100;
+								{ok,CountJsonStruct} ->
+									Size = lib_json:get_field(CountJsonStruct,"count")
+							end;
                         SizeParam ->
                             Size = list_to_integer(SizeParam)
                     end,
