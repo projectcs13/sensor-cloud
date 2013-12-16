a2a59faf3c650787d74da63551abaefa9d61215f
Taddic <tommy.mattsson@gmail.com>
2013-12-16 11:44:27 +0100

    Wrapper module for making http requests.

---
+++ test/http.erl
@@ -0,0 +1,30 @@
+%% @author Tommy Mattsson <tommy.mattsson@gmail.com>
+%% [www.csproj13.student.it.uu.se]
+%% @version 1.0
+%% @copyright [Copyright information]
+%%
+%% @doc == http ==
+%% This module contains several functions for making http request with webmachine
+%% in the module triggers which is done by calling the webbmachine.
+%%
+%% @end
+-module(http).
+-export([delete/1, get/1, post/2, put/2]).
+
+delete(Url) ->
+    {ok,{{_Vsn,Status,_Reason},_Hdrs,Body}} = httpc:request(delete,{Url,[]},[],[]),
+    {Status, Body}.
+
+get(Url) ->
+    {ok,{{_Vsn,Status,_Reason},_Hdrs,Body}} = httpc:request(get,{Url, []}, [], []),
+    {Status, Body}.
+
+
+
+post(Url, Request) ->
+    {ok,{{_Vsn,Status,_Reason},_Hdrs,Body}} = httpc:request(post,{Url,[],"application/json",Request},[],[]),
+    {Status, Body}.
+
+put(Url, Request) ->
+    {ok,{{_Vsn,Status _Reason},_Hdrs,Body}} = httpc:request(put,{Url,[],"application/json",Request},[],[]),
+    {Status, Body}.
