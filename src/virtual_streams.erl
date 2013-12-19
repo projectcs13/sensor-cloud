c41941df70fba4e94f27dacaf1bdfb448978c8f1
iakovosk <iakovosk90@gmail.com>
2013-12-19 16:04:14 +0100

    Improved time format in last_updated initiallization

--- src/virtual_streams.erl
+++ src/virtual_streams.erl
@@ -110,7 +110,9 @@ process_post(ReqData, State) ->
 									{{halt, 404}, wrq:set_resp_body("Wrong number of streams involved for diff", ReqData), State};
 								true ->	
 									Initialized1 = lib_json:add_field(DateAdded,"history_size",0),
-									Initialized2 = lib_json:add_field(Initialized1,"last_updated",list_to_binary(Date)),
+									{{Year,Month,Day},{Hour,Minute,Second}} = calendar:local_time(),
+									TimeStamp = binary:list_to_bin(api_help:generate_timestamp([Year,Month,Day,Hour,Minute,Second],0)),
+									Initialized2 = lib_json:add_field(Initialized1,"last_updated",TimeStamp),
 									case erlastic_search:index_doc(?INDEX, "virtual_stream", Initialized2) of	
 										{error, Reason} ->
 											{{error,Reason}, wrq:set_resp_body("{\"error\":\""++ atom_to_list(Reason) ++ "\"}", ReqData), State};
