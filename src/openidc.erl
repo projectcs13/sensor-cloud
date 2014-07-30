1723837c7bfb9d03141b0da562d2c18e2fafdd45
Alberto Blazquez <hola@albertoblazquez.net>
2014-07-30 02:25:59 +0200

    Small fixes. Develop authorizate_priviledged_users fuction

--- src/openidc.erl
+++ src/openidc.erl
@@ -8,21 +8,25 @@
 -module(openidc).
 -export([init/1,
          auth_gen_token/2,
+         authenticate_request/1,
          authenticate_token/2,
+         authorize_priviledge_request/1,
+         is_priviledge/1,
          exchange_token/2,
          fetch_user_info/0,
          process_auth_request/2,
          process_auth_redirect/2]).
-
-% -include("webmachine.hrl").
-% -include_lib("erlastic_search.hrl").
-% -include("field_restrictions.hrl").
 
 % TODO Grab these settings from a config file
 -define(APIKEY, "AIzaSyCyC23vutanlgth_1INqQdZsv6AgZRiknY").
 -define(CLIENT_ID, "995342763478-fh8bd2u58n1tl98nmec5jrd76dkbeksq.apps.googleusercontent.com").
 -define(CLIENT_SECRET, "fVpjWngIEny9VTf3ZPZr8Sh6").
 -define(REDIRECT_URL, "http://localhost:8000/users/_openid").
+
+-define(FRONTEND_ID, "107908217220817548513").
+-define(PUB_SUB_ID,  "<< add here ... >>").
+-define(POLLING_ID,  "<< add here ... >>").
+
 
 % %% @doc
 % %% Function: init/1
@@ -60,7 +64,11 @@ process_auth_redirect(ReqData, State) ->
 -spec auth_gen_token(Code::string(), AuthState::string()) -> string().
 auth_gen_token(Code, AuthState) ->
     {AccToken, RefToken} = exchange_token(Code, AuthState),
-    RefreshT = list_to_binary(string:substr(binary_to_list(RefToken), 3)),
+
+    RefreshT = case RefToken of
+        undefined -> list_to_binary("undefined");
+        String    -> list_to_binary(string:substr(binary_to_list(String), 3))
+    end,
 
     case AccToken of
         undefined -> {false, "Not possible to authenticate. Missing Access Token"};
@@ -69,9 +77,10 @@ auth_gen_token(Code, AuthState) ->
                 {error, _} -> {false, "Not possible to authenticate. Unreachable user info"};
 
                 {ok, UserData} ->
-                    case users:user_is_new(UserData) of
-                        false -> Status = users:replace_old_access_token(UserData, AccToken);
-                        true  -> Status = users:store_user(UserData, AccToken, RefreshT)
+                    Username = binary_to_list(proplists:get_value(<<"id">>, UserData)),
+                    Status = case users:user_is_new(Username) of
+                        false -> users:replace_access_token(Username, AccToken);
+                        true  -> users:store_user(UserData, AccToken, RefreshT)
                     end,
 
                     case Status of
@@ -87,24 +96,23 @@ auth_gen_token(Code, AuthState) ->
             end
     end.
 
+% TODO Test this function
 -spec authorize_priviledge_request(ReqData::tuple()) -> tuple().
 authorize_priviledge_request(ReqData) ->
-    case authenticate_token("refresh_token", ReqData) of
-        {error, Msg} -> {error, Msg};
-        {ok, Token}  ->
-            case wrq:get_req_header("Username", ReqData) of
-                undefined -> {error, "Not possible to perform the request. Missing 'Username'"};
-                Username when Username == "107908217220817548513" -> {ok, Token}
-            end
+    {JSON, _, _} = api_help:json_handler(ReqData, ""),
+    Username = binary_to_list(proplists:get_value(<<"user_id">>, JSON)),
+
+    case {is_priviledge(Username), authenticate_request(ReqData)} of
+        {false, _}         -> {error, "{\"error\": \"User not authorized\"}"};
+        {_, {error, Msg}}  -> {error, Msg};
+        {true, {ok, Data}} -> {ok, Data}
     end.
 
 
-% -spec is_priviledge(Username::string()) -> boolean().
-% is_priviledge(Username) ->
-%     Username == "107908217220817548513".
-%     % Username == "107908217220817548513" or % Frontend
-%     % Username == "107908217220817548513" or % Pub/Sub
-%     % Username == "107908217220817548513".   % Polling-System
+-spec is_priviledge(Username::string()) -> boolean().
+is_priviledge(Username) ->
+    % case Username of ?FRONTEND_ID or ?PUB_SUB_ID or ?POLLING_ID -> true; _ -> false end.
+    (Username == ?FRONTEND_ID) or (Username == ?PUB_SUB_ID) or (Username == ?POLLING_ID).
 
 
 -spec fetch_user_info() -> tuple().
@@ -127,8 +135,8 @@ authenticate_request(ReqData) -> authenticate_token("access_token", ReqData).
 -spec authenticate_token(TokenName::string(), ReqData::tuple()) -> tuple().
 authenticate_token(TokenName, ReqData) ->
     case wrq:get_req_header(TokenName, ReqData) of
-        undefined -> {error, "Not possible to perform the request. Missing " ++ TokenName};
-        TokenVal  -> check_valid_token(TokenName, TokenVal)
+        undefined -> {error, "{\"error\": \"Not possible to perform the request. Missing " ++ TokenName ++ "\"}"};
+        TokenVal  -> check_valid_token(TokenName, list_to_binary(TokenVal))
     end.
 
 
@@ -137,11 +145,11 @@ authenticate_token(TokenName, ReqData) ->
 -spec check_valid_token(TokenName::string(), TokenValue::string()) -> tuple().
 check_valid_token(TokenName, TokenValue) ->
     case verify_token(TokenValue) of
-        {error, Msg} -> {error, Msg};
-        {ok, JSON} ->
+        {error, Msg} -> {error, "{\"error\": \"" ++ Msg ++ "\"}"};
+        {ok, JSON}   ->
             case verify_token_response(TokenValue, JSON) of
                 {error, Error}        -> {error, Error};
-                {ok, Username, false} -> {error, "Delivered token not valid. Audience does not match the Client ID"};
+                {ok, Username, false} -> {error, "{\"error\": \"Token not valid. Expended by other system\"}"};
                 {ok, Username, true}  -> users:replace_access_token(Username, TokenValue)
             end
     end.
@@ -152,7 +160,7 @@ verify_token(Token) ->
     AuthURL = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" ++ Token,
     case plus_srv:get_url(httpc:request(get,{AuthURL,[]},[],[])) of
         {ok, Json} -> {ok, Json};
-        {error, _} -> {error, "Delivered token already expired"}
+        {error, _} -> {error, "Token not valid or already expired"}
     end.
 
 
