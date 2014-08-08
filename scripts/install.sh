cc5d734488b5d923b88b3209c1bc261b35b89870
iakovosk <iakovosk90@gmail.com>
2014-08-08 12:34:14 +0200

    Travis still not starting

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,9 @@ sudo apt-get install -yq xsltproc software-properties-common
 echo "#################################################################"
 echo "Installing Erlang"
 echo "#################################################################"
-wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_17.1-1~ubuntu~precise_amd64.deb
-sudo dpkg -i esl-erlang_17.1.1~ubuntu~precise_amd64.deb
-rm esl-erlang_17.1.1~ubuntu~precise_amd64.deb
+-wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_16.b.1~ubuntu~precise_amd64.deb
+-sudo dpkg -i esl-erlang_16.b.1~ubuntu~precise_amd64.deb
+-rm esl-erlang_16.b.1~ubuntu~precise_amd64.deb
 
 echo "#################################################################"
 echo "Installing Nodejs together with npm"
